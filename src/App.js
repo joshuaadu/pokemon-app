36d2ac7407440f01e10ae51d64ab7d8e87a03ebf
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-01-01 13:44:34 +0000

    fetch Pokemon list Fetch default 20 Pokemon at a time and fetch 20 more with every "Show more Pokemon" button click

--- src/App.js
+++ src/App.js
@@ -4,27 +4,33 @@ import PokemonCard from "./components/Pokemon/PokemonCard";
 import Pokemons from "./components/Pokemon/Pokemons";
 
 export default function App() {
-  const [showPokemon, setShow] = React.useState(false);
-  const [pokemon, setPokemon] = React.useState([]);
+  const [pokemonUrl, setPokemonUrl] = React.useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+  const [pokemonList, setPokemonList] = React.useState([]);
 
   React.useEffect(() => {
     (async () => {
-      const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const res = await fetch(pokemonUrl);
       const data = await res.json();
-      setPokemon(data);
-      console.log(data, "working");
+      setPokemonList(data);
     })();
-  }, []);
+  }, [pokemonUrl]);
+  console.log(pokemonList, "working");
+
+  const getNextPokemonList = () => {
+    setPokemonUrl(pokemonList.next);
+  };
 
   return (
     <div className="App">
       <h1>Pokemon Random Generator</h1>
-      <button onClick={() => setShow((prev) => !prev)}>Show me!</button>
-      <div>{showPokemon ? "Showing" : "hidden"}</div>
-      {/* <div>{pokemon}</div> */}
-      <Pokemons>
+      <Pokemons list={pokemonList.results}>
         <PokemonCard></PokemonCard>
       </Pokemons>
+      <button onClick={getNextPokemonList}>Show more Pokemons!</button>
     </div>
   );
 }
+
+// {/* <button onClick={() => setShow((prev) => !prev)}>Show me!</button> */}
