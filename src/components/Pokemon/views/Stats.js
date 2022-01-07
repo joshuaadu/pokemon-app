365320fa814b99b96748be24b11ec61705bf7ee4
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-01-07 21:46:16 +0000

    Change overview view font size and add Stats view

---
+++ src/components/Pokemon/views/Stats.js
@@ -0,0 +1,8 @@
+import classes from "./Stats.module.css";
+import viewCSS from "../PokemonCard.module.css";
+
+const Stats = (props) => {
+  return <div className={viewCSS.view}></div>;
+};
+
+export default Stats;
